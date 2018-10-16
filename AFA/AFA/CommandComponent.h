8353165637d873d0fe2bf2d6720eadcb75440eb8
Jack Keogh <keeeogh@gmail.com>
2018-10-16 16:35:17 +0100

    AFA_COM_04 Complete

---
+++ AFA/AFA/CommandComponent.h
@@ -0,0 +1,60 @@
+/// <summary>
+/// This component will handle commands made by the user and AI.
+/// </summary>
+
+#pragma once
+#include "stdafx.h"
+#include <ECS.h>
+#include "TransformComponent.h"
+
+class Command
+{
+public:
+	// Destructor
+	virtual ~Command() {};
+
+	/// <summary>
+	/// Execute
+	/// 
+	/// This function executes the necessary update for
+	/// the command.
+	/// </summary>
+	virtual void Execute(TransformComponent * T) {};
+
+protected:
+	// Constructor
+	Command() {};
+};
+
+class CommandComponent : public jk::Component
+{
+public:
+	// Constructors
+	CommandComponent() {};
+
+	/// <summary>
+	/// Init
+	/// 
+	/// The initialiser function for the Command Component.
+	/// </summary>
+	void Init() override
+	{
+
+	}
+
+	/// <summary>
+	/// Get Command
+	/// 
+	/// This getter functions returns a particular command
+	/// from the vector of commands.
+	/// </summary>
+	/// <param name="c">The command to return.</param>
+	/// <returns>A Command ptr.</returns>
+	Command * getCommand(string c)
+	{
+		return m_commands[c];
+	}
+
+private:
+	map <string, Command*> m_commands;
+};
