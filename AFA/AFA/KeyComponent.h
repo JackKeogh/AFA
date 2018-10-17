18f6bb28d24556519ebe6d7aa2d4bce97c2f589d
Jack Keogh <keeeogh@gmail.com>
2018-10-17 14:14:49 +0100

    AFA_COM_06 KeyComponent Class Created

--- AFA/AFA/KeyComponent.h
+++ AFA/AFA/KeyComponent.h
@@ -1,1 +1,16 @@
+/// <summary>
+/// This class will be used to bind a users choice of
+/// keys to keycodes.
+/// </summary>
+
 #pragma once
+#include "stdafx.h"
+#include <ECS.h>
+
+class KeyComponent : public jk::Component
+{
+public:
+
+private:
+
+};
