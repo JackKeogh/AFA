378b632eb083899e33b676db4a7dda36f85d87f9
Jack Keogh <keeeogh@gmail.com>
2018-10-17 17:09:09 +0100

    AFA_INP_02 No Errors

--- AFA/AFA/TestScene.h
+++ AFA/AFA/TestScene.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "Scene.h"
+#include "InputSystem.h"
 
 class TestScene : public Scene
 {
@@ -7,6 +8,10 @@ public:
 	TestScene() 
 	{
 		RenderSystem::RenderColor(SDL_Color{ 0, 0, 0, 255 });
+
+		m_running = true;
+		
+		m_inputSystem = new InputSystem;
 	};
 	~TestScene() {};
 
@@ -17,11 +22,25 @@ public:
 
 		RenderSystem::Present();
 	};
-	void Event() override {};
+
+	void Event() override 
+	{
+		SDL_Event LocalEvent;
+
+		while (SDL_PollEvent(&LocalEvent))
+		{
+			if (LocalEvent.type == SDL_QUIT)
+			{
+				m_running = false;
+				break;
+			}
+		}
+	};
+
 	bool Running() override { return m_running; };
 	void Initialise() override {};
 	void LoadLevel() override {};
 
 private:
-
+	InputSystem * m_inputSystem;
 };
