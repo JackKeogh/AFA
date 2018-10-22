1bba9611fae900607441f1ca2088b5fffb214529
Jack Keogh <keeeogh@gmail.com>
2018-10-22 16:53:38 +0100

    AFA_COM_07 Comments added

--- AFA/AFA/Rigidbody.h
+++ AFA/AFA/Rigidbody.h
@@ -11,31 +11,61 @@
 class RigidbodyComponent : public Component
 {
 public:
+	/// <summary>
+	/// Rigidbody Component
+	/// 
+	/// Default constructor.
+	/// </summary>
 	RigidbodyComponent()
 	{
-
 	};
 
+	/// <summary>
+	/// ~Rigidbody Component
+	/// 
+	/// Default destuctor.
+	/// </summary>
 	~RigidbodyComponent()
 	{
-
 	};
 
+	/// <summary>
+	/// Init
+	/// 
+	/// Override for initialiser function of component class.
+	/// </summary>
 	void Init() override
 	{
 
 	};
 
+	/// <summary>
+	/// Update
+	/// 
+	/// Override for update function of component class.
+	/// </summary>
 	void Update() override
 	{
 
 	};
 
+	/// <summary>
+	/// Using Gravity
+	/// 
+	/// Checks if the component has gravity turned on or off.
+	/// </summary>
+	/// <returns>A bool value.</returns>
 	bool UsingGravity()
 	{
 
 	};
 
+	/// <summary>
+	/// Get Collider
+	/// 
+	/// A getter function to get the collider.
+	/// </summary>
+	/// <returns>A SDL_Rect value.</returns>
 	SDL_Rect getCollider()
 	{
 
