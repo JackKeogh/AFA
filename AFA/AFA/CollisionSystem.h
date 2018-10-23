ffe6ca360e4caecd257ecc05b4ba059e297cbdf0
Jack Keogh <keeeogh@gmail.com>
2018-10-23 14:01:49 +0100

    AFA_COL_03 Changes made needs testing

--- AFA/AFA/CollisionSystem.h
+++ AFA/AFA/CollisionSystem.h
@@ -11,5 +11,6 @@ class CollisionSystem
 {
 public:
 	static void TileCollision(vector<jk::Entity*>& tiles, vector<jk::Entity*>& entities);
+	static bool CheckBelow(SDL_Rect A, SDL_Rect B);
 	static bool AABB(SDL_Rect A, SDL_Rect B);
 };
