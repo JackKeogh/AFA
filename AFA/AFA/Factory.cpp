46e02dab2b0a29a8edaaed1ee75cd81cd6c79c6a
Jack Keogh <keeeogh@gmail.com>
2018-10-26 18:13:10 +0100

    AFA_FAC_02 Small fix made

--- AFA/AFA/Factory.cpp
+++ AFA/AFA/Factory.cpp
@@ -6,7 +6,7 @@ void TileFactory::CreateEntity(EntityManager * EManager, const char * Texture, f
 	auto& ent = EManager->addEntity();
 	ent.addComponent<TransformComponent>(Vector2f(x, y), 60, 64);
 	ent.addComponent<SpriteComponent>(Texture, w, h);
-	ent.addComponent<RigidbodyComponent>(false);
+	ent.addComponent<RigidbodyComponent>(false, 2.0f);
 	ent.addLayer(jk::Layers::Middleground);
 	ent.addGroup(jk::Groups::TileGroup);
 }
