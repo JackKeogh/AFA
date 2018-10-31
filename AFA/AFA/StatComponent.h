18a89992cbdf2260a6423b21e726177291b67d24
Jack Keogh <keeeogh@gmail.com>
2018-10-31 11:45:30 +0000

    AFA_COM_08 Component Created

---
+++ AFA/AFA/StatComponent.h
@@ -0,0 +1,16 @@
+/// <summary>
+/// This component keeps track of an entities lives
+/// and heat variables
+/// </summary>
+
+#pragma once
+#include "stdafx.h"
+#include <ECS.h>
+
+class StatComponent : public jk::Component
+{
+public:
+
+private:
+
+};
