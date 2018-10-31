f5e6cfbb00647127d849e683f7f1b0294ba94f52
JasonHannon <jasonhannon1995@gmail.com>
2018-10-31 20:38:28 +0000

    Player Factory Added and Player Created Using Factory

--- AFA/AFA/Factory.cpp
+++ AFA/AFA/Factory.cpp
@@ -10,3 +10,15 @@ void TileFactory::CreateEntity(EntityManager * EManager, const char * Texture, f
 	ent.addLayer(jk::Layers::Middleground);
 	ent.addGroup(jk::Groups::TileGroup);
 }
+
+void PlayerFactory::CreateEntity(EntityManager * EManager, const char * Texture, float x, float y, float w, float h)
+{
+	auto& ent = EManager->addEntity();
+	ent.addComponent<TransformComponent>(Vector2f(x, y), 50, 80, 0, 1, 5, 5);
+	ent.addComponent<SpriteComponent>(Texture, w, h);
+	ent.addComponent<CommandComponent>();
+	ent.addComponent<KeyComponent>();
+	ent.addComponent<RigidbodyComponent>(true, 5.0f);
+	ent.addGroup(jk::Groups::PlayerGroup);
+	ent.addLayer(jk::Layers::Foreground);
+}
