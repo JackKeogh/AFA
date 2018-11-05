c5d4f5aefb9678bafdf2812ec876abe1b6024505
Jack Keogh <keeeogh@gmail.com>
2018-11-05 16:52:37 +0000

    AFA_FAC_03 Working

--- AFA/AFA/Factory.cpp
+++ AFA/AFA/Factory.cpp
@@ -6,7 +6,7 @@ void TileFactory::CreateEntity(EntityManager * EManager, const char * Texture, f
 	auto& ent = EManager->addEntity();
 	ent.addComponent<TransformComponent>(Vector2f(x, y), 60, 64);
 	ent.addComponent<SpriteComponent>(Texture, w, h);
-	ent.addComponent<RigidbodyComponent>(false, 2.0f);
+	ent.addComponent<RigidbodyComponent>(false, 1.0f);
 	ent.addLayer(jk::Layers::Middleground);
 	ent.addGroup(jk::Groups::TileGroup);
 }
@@ -14,11 +14,11 @@ void TileFactory::CreateEntity(EntityManager * EManager, const char * Texture, f
 void PlayerFactory::CreateEntity(EntityManager * EManager, const char * Texture, float x, float y, float w, float h)
 {
 	auto& ent = EManager->addEntity();
-	ent.addComponent<TransformComponent>(Vector2f(x, y), 50, 80, 0, 1, 5, 5);
+	ent.addComponent<TransformComponent>(Vector2f(x, y), 32, 32, 0, 1, 5, 5);
 	ent.addComponent<SpriteComponent>(Texture, w, h);
 	ent.addComponent<CommandComponent>();
 	ent.addComponent<KeyComponent>();
-	ent.addComponent<RigidbodyComponent>(true, 5.0f);
+	ent.addComponent<RigidbodyComponent>(true, 1.0f);
 	ent.addGroup(jk::Groups::PlayerGroup);
-	ent.addLayer(jk::Layers::Foreground);
+	ent.addLayer(jk::Layers::Middleground);
 }
